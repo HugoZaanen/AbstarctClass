e188ba70a97630e8dde3fa0a807224460221a22b
Huego <hugozaanen@hotmail.com>
2019-03-04 16:35:07 +0100

    abstract classes

---
+++ AbstarctClassTutorial/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstarctClassTutorial
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * (Math.Pow(Radius, 2.0));
+        }
+    }
+}
